e44c7498b29bc2958e3c1d2ab840a4ca761823c9
grek23 <grek23@gmail.com>
2026-03-08 21:31:49 -0700

    changing href in window.locaiton for renderweb app.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function generate() {
     //window.location.href = "https://your-api-url/worksheet";
-    window.location.href = "http://127.0.0.1:5000/worksheet";
+    //window.location.href = "http://127.0.0.1:5000/worksheet";
+    window.location.href = "https://mad-min.onrender.com/worksheet";
 }
