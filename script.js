156a8a138994a4567d78b85f435453899f240645
grek23 <grek23@gmail.com>
2026-03-19 07:33:57 -0700

    needed to make the urls match Render location.

--- script.js
+++ script.js
@@ -11,10 +11,14 @@ function generate() {
     //const max = document.getElementById("max").value;
     //const problems = document.getElementById("problems").value;
 
-    const url =
-    "http://127.0.0.1:5000/worksheet" + "?r";
+    //for local processing
+    //const url = "http://127.0.0.1:5000/worksheet" + "?r";
     //+ "?max=" + max
     //+ "&problems=" + problems;
+
+    //For processing on Render
+    const url = "https://mad-min.onrender.com/worksheet" + "?r";
+
 
     window.location.href = url;
 }
@@ -24,10 +28,12 @@ function generate_single_digit(single_digit) {
     //const single_digit = document.getElementById("single_digit").value;
     //const problems = document.getElementById("problems").value;
     //"/worksheet_single_digit"
-    const url =
-    "http://127.0.0.1:5000/worksheet_single_digit"
-    + "?single_digit=" + single_digit;
+
+    //for local processing
+    //const url = "http://127.0.0.1:5000/worksheet_single_digit" + "?single_digit=" + single_digit;
     
+    //For processing on render
+    const url = "https://mad-min.onrender.com/worksheet_single_digit" + "?single_digit=" + single_digit;
 
     window.location.href = url;
 }
